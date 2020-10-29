be91ae62cb48640392869351329edaec616f965d
Raneil <raneil.chavez1@gmail.com>
2020-10-29 16:12:03 +0800

    ver1.2.1

--- view/seed-record.php
+++ view/seed-record.php
@@ -397,7 +397,7 @@
   <div class="modal-dialog modal-xl" role="document">
     <div class="modal-content">
       <div class="modal-header">
-          <h5 class="modal-title" id="myModalLabel">SEED CONSIGNEE</h5>
+          <h5 class="modal-title" id="myModalLabel">SEED RECORD OTHER</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
       </div>
       <div class="modal-body">
